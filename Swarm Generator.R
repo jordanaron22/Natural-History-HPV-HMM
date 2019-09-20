f8f809a83df8bd2dc19b0b041de55761d5d5fc5d
jordanaron22 <jordanaron22@gmail.com>
2019-09-20 11:13:54 -0400

    Generates Swarm

--- Swarm Generator.R
+++ Swarm Generator.R
@@ -1,12 +1,8 @@
-n <- 100
-n2 <- 100
-name <- "ThreeSimsFullData.swarm"
+n <- 2500
+name <- "EM18Boot2.swarm"
 x <- ""
-for (i in 1:n){
-  x <- paste(x,"module load R; Rscript ThreeSimsFullData.R ",i,";\n", sep = "")
-  # for (j in 1:n2){
-  #   x <- paste(x,"module load R; Rscript ThreeSimsFullData.R ",i," ",j,";\n", sep = "")
-  # }
+for (i in 2001:n){
+  x <- paste(x,"module load R; Rscript EM18Boot.R ",i,";\n", sep = "")
 }
 
 writeLines(x, name)
